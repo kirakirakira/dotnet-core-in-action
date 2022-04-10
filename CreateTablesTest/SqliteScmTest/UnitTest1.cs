9da86495262415b24eb19ef8f871b26e4568f78e
Kira Hartlage <kira.hartlage@gmail.com>
2022-04-10 15:00:40 -0400

    set up test; just parttype table

--- CreateTablesTest/SqliteScmTest/UnitTest1.cs
+++ CreateTablesTest/SqliteScmTest/UnitTest1.cs
@@ -1,18 +1,26 @@
 using Xunit;
+using System.Linq;
+using WidgetScmDataAccess;
 
 namespace SqliteScmTest;
 
 public class UnitTest1 : IClassFixture<SampleScmDataFixture>
 {
     private SampleScmDataFixture fixture;
+    private ScmContext context;
     public UnitTest1(SampleScmDataFixture fixture)
     {
         this.fixture = fixture;
+        this.context = new ScmContext(fixture.Connection);
     }
 
     [Fact]
     public void Test1()
     {
-        Assert.True(true);
+        var parts = context.Parts;
+        Assert.Single(parts);
+
+        var part = parts.First();
+        Assert.Equal("8289 L-shaped plate", part.Name);
     }
 }
