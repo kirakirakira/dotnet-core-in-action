50c43510589763839d857761bee1854d06d5e513
Kira Hartlage <kira.hartlage@gmail.com>
2022-04-10 14:42:47 -0400

    sql!

---
+++ SqliteConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace SqliteConsoleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("Data Source=:memory:"))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand(
+                    "SELECT 1;", connection);
+
+                long result = (long)command.ExecuteScalar();
+                Console.WriteLine($"Command output: {result}");
+            }
+        }
+    }
+}
